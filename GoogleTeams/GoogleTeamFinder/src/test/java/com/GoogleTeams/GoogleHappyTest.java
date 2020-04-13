260d0985ee390f75c923ee2236c8aebef59f370a
mct15c <34050407+mct15c@users.noreply.github.com>
2020-04-12 23:14:44 -0500

    20 Tests added

--- GoogleTeams/GoogleTeamFinder/src/test/java/com/GoogleTeams/GoogleHappyTest.java
+++ GoogleTeams/GoogleTeamFinder/src/test/java/com/GoogleTeams/GoogleHappyTest.java
@@ -15,6 +15,9 @@ import org.junit.*;
  */
 public class GoogleHappyTest 
 {
+	
+	private int rowNumber;
+	private int columnNumber;
     GoogleHappy teams;
 	
 	@Rule 
@@ -40,14 +43,229 @@ public class GoogleHappyTest
 		
         //assertTrue( true );
     }
-}
 	
-/*	@Test
-	public void test2PageRank()
-	{
-		boolean worked = teams.
+	@Test
+    public void test2ReadFile()
+    {
+		boolean worked = teams.readFile("Pref_Inputs2.csv");
 		assertEquals(true, worked);
+		System.out.println("\n");
+		
+		
+        //assertTrue( true );
+    }
+	
+	@Test
+    public void test3ReadFile()
+    {
+		boolean worked = teams.readFile("Pref_Inputs3.csv");
+		assertEquals(true, worked);
+		System.out.println("\n");
+		
+		
+        //assertTrue( true );
+    }
+	
+		@Test
+    public void test4ReadFile()
+    {
+		boolean worked = teams.readFile("Pref_Inputs4.csv");
+		assertEquals(true, worked);
+		System.out.println("\n");
+		
+		
+        //assertTrue( true );
+    }
+	
+	@Test
+	public void test4MatrixRows()
+	{
+		boolean worked = teams.readFile("Pref_Inputs.csv");
+		assertEquals(true, worked);
+		int numRows = teams.getRowSize();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numRows);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test5MatrixRows()
+	{
+		boolean worked = teams.readFile("Pref_Inputs.csv");
+		assertEquals(true, worked);
+		int numColumns = teams.getColumnSize();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numColumns);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test6MatrixRows()
+	{
+		boolean worked = teams.readFile("Pref_Inputs2.csv");
+		assertEquals(true, worked);
+		int numRows = teams.getRowSize();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numRows);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test7MatrixRows()
+	{
+		boolean worked = teams.readFile("Pref_Inputs2.csv");
+		assertEquals(true, worked);
+		int numColumns = teams.getColumnSize();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numColumns);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test8MatrixRows()
+	{
+		boolean worked = teams.readFile("Pref_Inputs3.csv");
+		assertEquals(true, worked);
+		int numRows = teams.getRowSize();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numRows);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test9MatrixRows()
+	{
+		boolean worked = teams.readFile("Pref_Inputs3.csv");
+		assertEquals(true, worked);
+		int numColumns = teams.getColumnSize();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numColumns);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test10MatrixRows()
+	{
+		boolean worked = teams.readFile("Pref_Inputs4.csv");
+		assertEquals(true, worked);
+		int numRows = teams.getRowSize();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numRows);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test11MatrixRows()
+	{
+		boolean worked = teams.readFile("Pref_Inputs4.csv");
+		assertEquals(true, worked);
+		int numColumns = teams.getColumnSize();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numColumns);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test1ZeroCheck()
+	{
+		boolean worked = teams.readFile("Pref_Inputs.csv");
+		assertEquals(true, worked);
+		int numRows = teams.zeroChecker();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numRows);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test1OneCheck()
+	{
+		boolean worked = teams.readFile("Pref_Inputs.csv");
+		assertEquals(true, worked);
+		int numColumns = teams.oneChecker();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numColumns);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test2ZeroCheck()
+	{
+		boolean worked = teams.readFile("Pref_Inputs2.csv");
+		assertEquals(true, worked);
+		int numRows = teams.zeroChecker();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numRows);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test2OneCheck()
+	{
+		boolean worked = teams.readFile("Pref_Inputs2.csv");
+		assertEquals(true, worked);
+		int numColumns = teams.oneChecker();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numColumns);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test3ZeroCheck()
+	{
+		boolean worked = teams.readFile("Pref_Inputs3.csv");
+		assertEquals(true, worked);
+		int numRows = teams.zeroChecker();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numRows);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test3OneCheck()
+	{
+		boolean worked = teams.readFile("Pref_Inputs3.csv");
+		assertEquals(true, worked);
+		int numColumns = teams.oneChecker();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numColumns);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test4ZeroCheck()
+	{
+		boolean worked = teams.readFile("Pref_Inputs4.csv");
+		assertEquals(true, worked);
+		int numRows = teams.zeroChecker();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numRows);
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void test4OneCheck()
+	{
+		boolean worked = teams.readFile("Pref_Inputs4.csv");
+		assertEquals(true, worked);
+		int numColumns = teams.oneChecker();
+		//System.out.println("Number of rows in matrix: " +numRows);
+		assertEquals(true, worked);
+		assertEquals(0,numColumns);
 		System.out.println("\n");
 	}
 }
-*/
